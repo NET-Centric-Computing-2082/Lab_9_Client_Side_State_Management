32b6de46899ba5ed8a202d21a5f8387fcc148441
Nabin Kandel <nabin.kandel@oic.edu.np>
2025-08-20 19:52:31 +0545

    Add project files.

---
+++ Lab_9_Client_Side_State_Management/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab_9_Client_Side_State_Management.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
